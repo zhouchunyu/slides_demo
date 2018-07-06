8b1ee042a599a68eead954b84328ebd87aecbe4b
zhouchunyu <zhouchunyu20xx@163.com>
2018-07-06 15:25:22 +0800

    git init

---
+++ main.js
@@ -0,0 +1,21 @@
+allButtons = $(".buttons > button")
+allButtons.each(function(index, btn){
+  $(btn).on("click", function(event){
+    distance = $(event.currentTarget).index() * 300;
+    $('.images').css({transform: `translateX(-${distance}px)`});
+  });
+});
+
+var n = 0;
+var size = allButtons.length;
+trigger_button(allButtons, n, size);
+setInterval(() => {
+  n += 1;
+  trigger_button(allButtons, n, size);
+}, 1000);
+
+function trigger_button(buttons, n, size) {
+  buttons.eq(n%size).trigger('click');
+  buttons.removeClass('red');
+  buttons.eq(n%size).addClass('red');
+}
